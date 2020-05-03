5e8292a28191c24e1cf845abb84b8de990a57214
Preoo <Preoo@users.noreply.github.com>
2020-05-03 13:20:23 +0300

    Add exercises 1.3 -> 1.5 as single solution.

--- Part_1/courseinfo/src/index.tsx
+++ Part_1/courseinfo/src/index.tsx
@@ -1,52 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props:any) => (
-  <h1>{props.course}</h1>
-)
-
-type CourseContent = {
-  parts:string[],
-  exercises:number[],
-}
-const Content = ({parts, exercises}:CourseContent) => {
-  if (parts.length !== exercises.length) return <div><p>Malformed input</p></div>
-  const content = parts.map((part, i) => <Part part={part} exercises={exercises[i]} />)
-  return (
-    <div>
-      {content}
-    </div>
-  )
-}
-
 type PartElement = {
-  part:string,
+  name:string,
   exercises:number,
 }
-const Part = ({part, exercises}:PartElement) => (
-  <p>{part} {exercises}</p>
+type Course = {
+  name:string,
+  parts:PartElement[],
+}
+
+const Header = ({name}:Course) => (
+  <h1>{name}</h1>
 )
 
-const Total = (props:any) => (
-  <p>Number of exercises: {props.exercises.reduce((sum:number, next:number) => sum + next)}</p>
-)
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+const Content = ({parts}:Course) => {
+  let course_content = []
+  for (const part of parts) {
+    course_content.push(<Part name={part.name} exercises={part.exercises} />)
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      {course_content}
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Part = ({name, exercises}:PartElement) => (
+  <p>{name} {exercises}</p>
+)
+
+const Total = ({parts}:Course) => {
+  let total:number = 0
+
+  parts.forEach((part) => total += part.exercises)
+
+  return (
+    <p>Number of exercises: {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ]
+  }
+
+  return (
+    <div>
+      <Header {...course} />
+      <Content {...course} />
+      <Total {...course} />
+    </div>
+  )
+}
+
+ReactDOM.render
+(
+  <App />,
+  document.getElementById('root')
+)
