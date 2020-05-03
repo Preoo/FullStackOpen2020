708ec6b859d0521dc7fcf6b61bec44ef645ca12d
Preoo <Preoo@users.noreply.github.com>
2020-05-03 14:42:08 +0300

    Add exercise 1.6

--- Part_1/unicafe/src/index.tsx
+++ Part_1/unicafe/src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Rating = (props:any) => <li>{props.rating} -- {props.count}</li>
+const Display = (props:any) => (
+  <ul>
+    <Rating rating="good" count={props.good} />
+    <Rating rating="neutral" count={props.neutral} />
+    <Rating rating="bad" count={props.bad} />
+  </ul>
+)
+const RateButton = (props:any) => (
+  <button onClick={props.cb}>
+    {props.rating}
+  </button>
+)
+const App = () => {
+  // save clicks of each button to own state
+  const [good, set_good] = useState(0)
+  const [neutral, set_neutral] = useState(0)
+  const [bad, set_bad] = useState(0)
+
+  return (
+    <div>
+      <h1>Rate unicafe</h1>
+      <RateButton rating="good" cb={() => set_good(bad + 1)} />
+      <RateButton rating="neutral" cb={() => set_neutral(bad + 1)} />
+      <RateButton rating="bad" cb={() => set_bad(bad + 1)} />
+      <h2>Statistics</h2>
+      <Display good={good} neutral={neutral} bad={bad}/>
+
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
