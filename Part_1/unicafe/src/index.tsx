e1de777ac992d0fa163d59619a9ad5be6b629b08
Preoo <Preoo@users.noreply.github.com>
2020-05-03 15:04:02 +0300

    Add exercises 1.7, 1.8

--- Part_1/unicafe/src/index.tsx
+++ Part_1/unicafe/src/index.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Rating = (props:any) => <li>{props.rating} -- {props.count}</li>
-const Display = (props:any) => (
-  <ul>
-    <Rating rating="good" count={props.good} />
-    <Rating rating="neutral" count={props.neutral} />
-    <Rating rating="bad" count={props.bad} />
-  </ul>
-)
+const Rating = (props:any) => <li>{props.label} -- {props.value}</li>
+const Metrics = (props:any) => {
+  const {good, neutral, bad} = props
+  const total = good + neutral + bad
+  const avg = (good - bad) / (total)
+  const positive = (good / total) * 100.0
+  return (
+    <ul>
+      <Rating label="good" value={good} />
+      <Rating label="neutral" value={neutral} />
+      <Rating label="bad" value={bad} />
+      <Rating label="all" value={total} />
+      <Rating label="average" value={avg} />
+      <Rating label="positive" value={positive} />
+    </ul>
+  )
+}
 const RateButton = (props:any) => (
   <button onClick={props.cb}>
     {props.rating}
   </button>
 )
+
 const App = () => {
   // save clicks of each button to own state
   const [good, set_good] = useState(0)
@@ -23,12 +33,11 @@ const App = () => {
   return (
     <div>
       <h1>Rate unicafe</h1>
-      <RateButton rating="good" cb={() => set_good(bad + 1)} />
-      <RateButton rating="neutral" cb={() => set_neutral(bad + 1)} />
+      <RateButton rating="good" cb={() => set_good(good + 1)} />
+      <RateButton rating="neutral" cb={() => set_neutral(neutral + 1)} />
       <RateButton rating="bad" cb={() => set_bad(bad + 1)} />
       <h2>Statistics</h2>
-      <Display good={good} neutral={neutral} bad={bad}/>
-
+      <Metrics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
