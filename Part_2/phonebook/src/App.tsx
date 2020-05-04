0bb19e3f91d58a1d7b82fab4b5903d92202a4f4e
Preoo <Preoo@users.noreply.github.com>
2020-05-04 17:04:57 +0300

    Add exercise 2.8

--- Part_2/phonebook/src/App.tsx
+++ Part_2/phonebook/src/App.tsx
@@ -1,23 +1,41 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '123-4567890' }
   ]) 
-  const [ newName, setNewName ] = useState('')
-
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  
+  type Contact = {name:string, number:string}
+  // Filters
+  const filter_existing = (phonebook:Contact[], contact:Contact): boolean =>
+    phonebook.find(person => person.name === contact.name) === undefined
+  const filter_has_number = (phonebook:Contact[], contact:Contact): boolean =>
+    contact.number.length ? true : false
+  
   const add_person = (event:any) => {
     event.preventDefault()
-    if (persons.find(person => person.name === newName)) {
-      alert(`${newName} is already recorded`)
+    const filters = [filter_existing, filter_has_number]
+    const messages = [`${newName} is already recorded`, `number-field cannot by empty`]
+    const contact:Contact = {name: newName, number: newNumber}
+    const errors = filters.map(f => f(persons, contact))
+
+    if (errors.every(err => err)) {
+      setPersons(persons.concat(contact))
     } else {
-      setPersons([...persons, {name: newName}])
+      errors.forEach((ok, i) => {if (!ok) alert(messages[i])})
     }
     setNewName('')
+    setNewNumber('')
   }
 
+  // Event handlers
   const on_name_change = (event:any) => (
     setNewName(event.target.value)
+  )
+  const on_number_change = (event:any) => (
+    setNewNumber(event.target.value)
   )
 
   return (
@@ -26,14 +44,15 @@ const App = () => {
       <form onSubmit={add_person}>
         <div>
           name: <input value={newName} onChange={on_name_change}/>
+          number: <input value={newNumber} onChange={on_number_change}/>
         </div>
         <div>
-          <button type="submit">add: {newName}</button>
+          <button type="submit">add</button>
         </div>
       </form>
-      <h2>Names</h2>
-      {persons.map(person =>
-        <p key={person.name}>{person.name}</p>
+      <h2>Numbers</h2>
+      {persons.map(contact =>
+        <p key={contact.name}>{contact.name} : {contact.number}</p>
       )}
     </div>
   )
