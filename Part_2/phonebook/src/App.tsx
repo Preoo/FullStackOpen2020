9fb6e73ab117ec83e3a04c56780df6a323861a98
Preoo <Preoo@users.noreply.github.com>
2020-05-04 16:14:46 +0300

    Add exercise 2.6

---
+++ Part_2/phonebook/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const add_person = (event:any) => {
+    event.preventDefault()
+    setPersons([...persons, {name: newName}])
+    setNewName('')
+  }
+  const on_name_change = (event:any) => (
+    setNewName(event.target.value)
+  )
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={add_person}>
+        <div>
+          name: <input value={newName} onChange={on_name_change}/>
+        </div>
+        <div>
+          <button type="submit">add: {newName}</button>
+        </div>
+      </form>
+      <h2>Names</h2>
+      {persons.map(person =>
+        <p>{person.name}</p>
+      )}
+    </div>
+  )
+}
+
+export default App
