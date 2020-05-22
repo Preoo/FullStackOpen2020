a1f85f1ac37602be3aaf0c8574053d6e00696a6f
Preoo <Preoo@users.noreply.github.com>
2020-05-22 15:58:03 +0300

    add exercises 3.5, 3.6

--- Part_3/phonebook-server/index.js
+++ Part_3/phonebook-server/index.js
@@ -3,16 +3,21 @@ const app = express()
 const base_api = '/api/persons/'
 let persons = [
     {
-      "name": "feelsgoodman",
-      "number": "clap",
-      "id": 1
+      name: "feelsgoodman",
+      number: 321,
+      id: 1
     },
     {
-      "name": "asd",
-      "number": "asd",
-      "id": 2
+      name: "asd",
+      number: 123,
+      id: 2
     }
 ]
+
+const get_random_id = (max) => Math.floor(Math.random() * Math.floor(max))
+
+// middlewares
+app.use(express.json())
 
 app.get('/', (req, res) => {
 	res.send('hello')
@@ -32,6 +37,26 @@ app.get(`${base_api}:id`, (req, res) => {
   const id = req.params.id
   const person = persons.find(person => person.id === +id)
   person ? res.send(person) : res.status(404).end()
+})
+
+app.post(`${base_api}`, (req, res) => {
+  const {name, number} = req.body || {}
+
+  if (!name || !number) return res.status(400).json(
+    { error:'content missing. A valid name and number is required.'})
+  
+  if (persons.find(person => person.name === name)) return res.status(400).json({
+    error: 'for some reason names must be unique 🤔'
+  })
+  
+  const new_person = {
+    name: name,
+    number: number,
+    id: get_random_id(Number.MAX_SAFE_INTEGER)
+  }
+  console.log(new_person)
+  persons = persons.concat(new_person)
+  res.json(new_person)
 })
 
 app.delete(`${base_api}:id`, (req, res) => {
