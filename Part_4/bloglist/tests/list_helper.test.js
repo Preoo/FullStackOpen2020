717e529c035cb7193a1953a62d06356aa82613de
Preoo <Preoo@users.noreply.github.com>
2020-05-29 18:44:21 +0300

    add exercises 4.4

--- Part_4/bloglist/tests/list_helper.test.js
+++ Part_4/bloglist/tests/list_helper.test.js
@@ -1,7 +1,25 @@
 const blogs = require('../utils/blogs_for_tests')
 const list_helper = require('../utils/list_helper')
+describe('list_helper misc', () => {
+    test('dummy returns constant', () => {
+        const res = list_helper.dummy([])
+        expect(res).toBe(1)
+    })
 
-test('dummy returns constant', () => {
-    const res = list_helper.dummy([])
-    expect(res).toBe(1)
+})
+
+describe('total likes', () => {
+    test('when list has only 1 entry, total likes should equal likes of that entry', () => {
+        const blog = blogs[blogs.length * Math.random() | 0] // sample random from blogs
+        expect(list_helper.total_likes([blog])).toBe(blog.likes)
+    })
+
+    test('when list has many blogs, return correct total', () => {
+        const correct = blogs.reduce((a, c) => a + c.likes, 0)
+        expect(list_helper.total_likes(blogs)).toBe(correct)
+    })
+
+    test('when list is empty, return 0', () => {
+        expect(list_helper.total_likes([])).toBe(0)
+    })
 })
--- Part_4/bloglist/utils/list_helper.js
+++ Part_4/bloglist/utils/list_helper.js
@@ -1,9 +1,22 @@
-
 // eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => {
+const dummy = blogs => {
     return 1
 }
 
+const total_likes = blogs => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favourite_blog = blogs => blogs
+
+const most_blogs = blogs => blogs
+
+const most_likes = blogs => blogs
+
 module.exports = {
-    dummy
+    dummy,
+    total_likes,
+    favourite_blog,
+    most_blogs,
+    most_likes
 }
