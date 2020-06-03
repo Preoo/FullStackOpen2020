46e1f45b9bbecc9fbcbee2de79f4c2a28095be17
Preoo <Preoo@users.noreply.github.com>
2020-06-03 15:29:15 +0300

    add exercise 5.7

--- Part_5/bloglist-frontend/src/components/Blog.js
+++ Part_5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,41 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} by {blog.author}
-    </div>
-)
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+    const [compact, SetCompact] = useState(true)
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const details = () => (
+        <div>
+            <p>
+            Url: {blog?.url}
+            </p>
+            <p>
+                Author: {blog.author}
+            </p>
+            <p>
+                Likes: {blog.likes} <button>like</button>
+            </p>
+        </div>
+
+    )
+    return (
+        <div style={blogStyle}>
+            <button onClick={() => SetCompact(!compact)}>
+                {compact ? 'more' : 'less'}
+            </button>
+            {blog.title}
+            {
+                compact === false
+                ? details()
+                : ' ..'
+            }
+        </div>
+    )
+}
 
 export default Blog
