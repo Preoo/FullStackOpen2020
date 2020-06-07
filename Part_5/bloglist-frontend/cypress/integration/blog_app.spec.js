138d60031c0b7174c9f7645bdbb330cee86545f0
Preoo <Preoo@users.noreply.github.com>
2020-06-07 15:49:47 +0300

    add exercise 5.20

--- Part_5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ Part_5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,7 @@
 describe('Blog list', () => {
     beforeEach(() => {
         cy.request('POST', 'http://localhost:3003/api/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', { user: 'test', username: 'test', password: 'test' })
         cy.visit('http://localhost:3000')
     })
     it('can open front page', () => {
@@ -10,9 +11,6 @@ describe('Blog list', () => {
     })
 
     describe('when not logged in', () => {
-        beforeEach(() => {
-            cy.request('POST', 'http://localhost:3003/api/users', { user: 'test', username: 'test', password: 'test' })
-        })
         it('logging in works with correct creds', () => {
             cy.get('#usernameInput').type('test')
             cy.get('#passwordInput').type('test')
@@ -32,7 +30,35 @@ describe('Blog list', () => {
             cy.contains('Invalid credentials')
         })
     })
-    // describe('when logged in', () => {
-
-    // })
+    describe('when logged in', () => {
+        beforeEach(() => {
+            cy.login({ username: 'test', password: 'test' })
+        })
+        it('user can post new blog', () => {
+            const blog = {
+                title: 'test_blog',
+                author: 'test_author',
+                url: 'test_url'
+            }
+            cy.contains('new blog').click()
+            cy.get('#addBlogForm').get('#inputTitle').type(blog.title)
+            cy.get('#addBlogForm').get('#inputAuthor').type(blog.author)
+            cy.get('#addBlogForm').get('#inputUrl').type(blog.url)
+            cy.contains('add').click()
+            cy.contains('added blog')
+            cy.contains('test_blog')
+        })
+        it('user can add a like to a blog', () => {
+            const blog = {
+                title: 'like_test_blog',
+                author: 'like_test_author',
+                url: 'like_test_url'
+            }
+            cy.createBlog(blog)
+            cy.contains('more').click()
+            cy.contains('Likes: 0')
+            cy.get('.likeButton').click()
+            cy.contains('Likes: 1')
+        })
+    })
 })
