bca2e42dedfda59b1498d21cced8a7d930eadb40
Preoo <Preoo@users.noreply.github.com>
2020-06-12 16:19:39 +0300

    add exercise 6.8

--- Part_6/redux-anecdotes/src/App.js
+++ Part_6/redux-anecdotes/src/App.js
@@ -1,41 +1,21 @@
 import React from 'react'
 import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { vote_anecdote, add_anecdote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import Anecdotes from './components/Anecdotes'
 
-const App = () => {
-    const anecdotes = useSelector(state => state)
-    const dispatch = useDispatch()
-    
-    const vote = id => dispatch(vote_anecdote(id))
-
-    return (
-        <article>
-            <header>
-                <h2>
-                    Anecdotes
-                </h2>
-            </header>
-            {anecdotes.map(anecdote => 
-                <section key={anecdote.id}>
-                    <p>
-                        {anecdote.content}
-                    </p>
-                    <p>
-                        has {anecdote.votes} votes
-                        <button onClick={() => vote(anecdote.id)}>
-                            vote
-                        </button>
-                    </p>
-                </section>
-            )}
-            <AnecdoteForm />
-            <footer>
-                footer block
-            </footer>
-        </article>
-    )
-}
+const App = () => (
+    <article>
+        <header>
+            <h2>
+                Anecdotes
+            </h2>
+        </header>
+        <Anecdotes />
+        <AnecdoteForm />
+        <footer>
+            footer text
+        </footer>
+    </article>
+)
 
 export default App
---
+++ Part_6/redux-anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { vote_anecdote } from '../reducers/anecdoteReducer'
+
+const Anecdotes = (props) => {
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+    const vote = id => dispatch(vote_anecdote(id))
+
+    return (
+        anecdotes.map(anecdote => 
+            <section key={anecdote.id}>
+                <p>
+                    {anecdote.content}
+                </p>
+                <p>
+                    has {anecdote.votes} votes
+                    <button onClick={() => vote(anecdote.id)}>
+                        vote
+                    </button>
+                </p>
+            </section>
+        )
+    )
+}
+
+export default Anecdotes
