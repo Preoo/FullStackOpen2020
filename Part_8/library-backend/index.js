48895068ba3078507f2184ee5f65fc1a6e076921
Preoo <Preoo@users.noreply.github.com>
2020-07-27 18:22:41 +0300

    complete exercise 8.3

--- Part_8/library-backend/index.js
+++ Part_8/library-backend/index.js
@@ -77,17 +77,24 @@ let books = [
 	}]
 
 const typeDefs = gql`
+	type Author {
+		name: String!
+		id: ID!
+		born: Int
+		bookCount: Int
+	}
 	type Book {
 		title: String!
 		published: Int!
 		author: String!
 		id: ID!
-		genres: [String!]!
+		genres: [String]!
 	}
 	type Query {
 		bookCount: Int!
 		authorCount: Int!
 		allBooks: [Book!]!
+		allAuthors: [Author]!
 	}
 `
 
@@ -95,7 +102,11 @@ const resolvers = {
 	Query: {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
-		allBooks: () => books
+		allBooks: () => books,
+		allAuthors: () => authors
+	},
+	Author: {
+		bookCount: (root) => books.filter(book => book.author === root.name).length
 	}
 }
 
