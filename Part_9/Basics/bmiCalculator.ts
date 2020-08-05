14897d249d7b28b6332e71a140a5e0b0ce46374c
Preoo <Preoo@users.noreply.github.com>
2020-08-05 18:11:35 +0300

    complete exercises 9.1 - 9.3

---
+++ Part_9/Basics/bmiCalculator.ts
@@ -0,0 +1,36 @@
+interface BMIConfig {
+    weight: number,
+    height: number
+}
+
+const parseArgs = (args:string[]): BMIConfig => {
+    if (args.length !== 4) throw new Error('Invalid argument exception: expected 4 args');
+    const weight = Number(args[2]);
+    const height = Number(args[3]);
+    if (isNaN(weight) || isNaN(height)) {
+        throw new Error('Argument type exception: values are not numbers');
+    }
+
+    return { weight, height };
+}
+
+const calculateBMI = (weight:number, height:number): string => {
+    const ratio = weight / Math.pow(height*0.01, 2);
+    if (ratio <= 15) return 'Very severely underweight';
+    if (15 < ratio && ratio <= 16) return 'Severely underweight';
+    if (16 < ratio && ratio <= 18.5) return 'Underweight';
+    if (18.5 < ratio && ratio <= 25) return 'Normal (healthy weight)';
+    if (25 < ratio && ratio <= 30) return 'Overweight';
+    if (30 < ratio && ratio <= 35) return 'Obese Class I (Moderately obese)';
+    if (35 < ratio && ratio <= 40) return 'Obese Class II (Severely obese)';
+    if (40 < ratio) return 'Obese Class III (Very severely obese)';
+}
+
+try {
+    const config = parseArgs(process.argv);
+    console.info(`BMI results for ${config.weight}kg and ${config.height}cm are
+        ${calculateBMI(config.weight, config.height)}
+    `);
+} catch (ex) {
+    console.error(`Ooops! ${ex.message}`);
+}
---
+++ Part_9/Basics/exerciseCalculator.ts
@@ -0,0 +1,56 @@
+interface ExerciseResults {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number 
+}
+
+interface ExercisesConfig {
+    target: number,
+    exerciseHours: number[]
+}
+
+const buildConfig = (args:string[]): ExercisesConfig => {
+    const [node, script, targetInput, ...exercisesInput] = args;
+    const target = Number(targetInput);
+    const exerciseHours = exercisesInput.map(Number).filter(n =>!isNaN(n));
+    if (isNaN(target) || !exerciseHours.length) {
+        throw new Error('Invalid arguments exception: Expected atleast 2 numerical arguments');
+    }
+
+    return { target, exerciseHours };
+}
+
+const getRating = (average:number, target: number): number => {
+    if (average >= target) return 3;
+    if (Math.abs(target - average) <= 1) return 2;
+    return 1;
+}
+
+const calculateExercises = (config:ExercisesConfig): ExerciseResults => {
+    const { target, exerciseHours } = config
+    const average = exerciseHours.reduce((sum, next) => sum + next, 0) / exerciseHours.length;
+    const success = average >= target;
+    const rating = getRating(average, target);
+    const descriptions = ['so bad', 'almost', 'veri gud'];
+
+    return {
+        periodLength: exerciseHours.length,
+        trainingDays: exerciseHours.reduce((t, d) => d > 0 ? t+1 : t, 0),
+        success,
+        rating,
+        ratingDescription: descriptions[rating - 1],
+        target,
+        average
+    };
+}
+
+try {
+    const config = buildConfig(process.argv);
+    console.info(calculateExercises(config));
+} catch (e) {
+    console.error(`Oooops! ${e.message}`);
+}
