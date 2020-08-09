aecd0325395cc83845ad576aeabc0cc2e5c049b6
Preoo <Preoo@users.noreply.github.com>
2020-08-09 14:18:02 +0300

    complete exercise 9.9.
    init Patientor frontend

--- Part_9/Patientor-server/src/index.ts
+++ Part_9/Patientor-server/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import Ping from './routes/ping';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', Ping);
+app.use('/api/ping', Ping);
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
--- Part_9/Patientor-server/src/routes/ping.ts
+++ Part_9/Patientor-server/src/routes/ping.ts
@@ -1,8 +1,7 @@
 import express from 'express';
-
 const router = express.Router();
 
-router.get('/ping', (_req, res) => {
+router.get('/', (_req, res) => {
 	res.send('pong');
 });
 
